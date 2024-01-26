5e16f13faafac2df3338ba2a974c7f9f474317cc
unknown <poberezhnaya.78@gmail.com>
2024-01-26 12:57:33 +0000

    Adding the files of the employees that I`m working on

---
+++ Folder/A-Team-Profile-Generator/Lib/intern.js
@@ -0,0 +1,11 @@
+const Employee = require("./employee"); //referencing to the parent class of an employee to expand it further
+class Intern extends Employee {
+  constructor(name, id, email, school) {
+    super(name, id, email); //inherit name,id,class from an Employee class
+    this.school = school;
+  }
+  getRole() {
+    return "Intern";
+  }
+}
+module.exports = Intern;
---
+++ Folder/A-Team-Profile-Generator/index.js
@@ -0,0 +1,6 @@
+import inquirer from 'inquirer'; // an easy way to capture user input
+var fs = require('fs');
+//tests
+const teamManager= require('./tests/teamManager.js');
+const engineer= require('./tests/engineer.js');
+const intern= require('./tests/intern.js');
